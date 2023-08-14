61993544d701a39dc3466b78a879aec2016c2990
Hirano Tomoya <ht.tomoya0605@gmail.com>
2023-08-14 02:36:10 +0000

    6-12-3終了

--- js/script.js
+++ js/script.js
@@ -4,7 +4,8 @@ const items = document.querySelectorAll('.img-item');
 for (let i = 0; i < items.length; i++) {
 const keyframes = {
   opacity: [0, 1],
-  translate: ['0 50px', 0],
+  rotate: ['5deg', 0],
+  scale: [5, 1],
 };
 const options = {
   duration: 600,
